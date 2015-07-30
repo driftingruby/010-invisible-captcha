19746e9498cf88b042a110f64d5ef16273072e50
kobaltz <dave@k-innovations.net>
2015-07-29 22:44:04 -0400

    prep

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,8 @@
 class VisitorsController < ApplicationController
+  def contact
+  end
+
+  def send_contact
+    redirect_to root_url, notice: 'Your message has been sent!'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get :contact, to: 'visitors#contact'
+  post :contact, to: 'visitors#send_contact'
   root to: 'visitors#index'
 end
